2a81ddc92740c77bed5e7ec76905003d8667da18
katie-sss <144366221+katie-sss@users.noreply.github.com>
2023-11-17 10:02:17 -0600

    Update MyV0Component.tsx

--- app/components/MyV0Component.tsx
+++ app/components/MyV0Component.tsx
@@ -1,62 +1,90 @@
-// you can overwrite this entire file with your v0 Component.
-// just copy and paste the "React" output over the entire file.
+/**
+ * v0 by Vercel.
+ * @see https://v0.dev/t/qHgdGHmrFqJ
+ */
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
-import Link from "next/link";
-import React from "react";
-import {V0Logo} from "./symbols";
-
-function TestComponent() {
+export default function Component() {
   return (
-    <div
-      style={{
-        display: "flex",
-        height: "100%",
-        justifyContent: "space-between",
-        flexDirection: "column",
-        width: "100%",
-      }}
-    >
-      <h3
-        style={{
-          maxWidth: "11em",
-        }}
-      >
-        This would be a pretty good place for a{" "}
-        <Link href="https://v0.dev/" target="_blank" rel="noopener noreferrer">
-          v0 component
-        </Link>
-        , wouldn't it?
-      </h3>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "flex-end",
-          width: "100%",
-        }}
-      >
-        <p
-          style={{
-            maxWidth: "20em",
-          }}
+    <div key="1" className="relative w-full max-w-md mx-auto p-5 rounded-lg shadow-md overflow-hidden">
+      <div className="absolute inset-0 bg-gradient-to-br from-blue-500 to-green-500 transform rotate-45" />
+      <div className="absolute inset-0 flex flex-wrap content-center justify-center bg-black">
+        <svg
+          className=" w-2 h-2 text-black opacity-10"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Go make one, then paste it into{" "}
-          <code
-            style={{
-              fontFamily: "var(--font-geist-mono)",
-              fontWeight: 500,
-              fontSize: "0.95em",
-              fontFeatureSettings: "'ss09'",
-            }}
-          >
-            app/components/MyV0Component.tsx
-          </code>
-        </p>
-
-        <V0Logo />
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+        </svg>
+        <svg
+          className=" w-2 h-2 text-black opacity-10 ml-10 mt-10"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+        </svg>
+        <svg
+          className=" w-2 h-2 text-black opacity-10 ml-20 mt-20"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+        </svg>
+        <svg
+          className=" w-2 h-2 text-black opacity-10 ml-30 mt-30"
+          fill="none"
+          height="24"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3Z" />
+        </svg>
+      </div>
+      <div className="relative bg-white dark:bg-coolGray-800 p-5 rounded-lg">
+        <div className="text-center space-y-2">
+          <h1 className="text-2xl font-semibold">Guest Book</h1>
+          <p className="text-gray-500 dark:text-gray-300">We would love to hear from you!</p>
+        </div>
+        <div className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="guest-name">Your Name</Label>
+            <Input id="guest-name" placeholder="Enter your name" required />
+          </div>
+          <div className="space-y-2" />
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
+        </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default TestComponent;
